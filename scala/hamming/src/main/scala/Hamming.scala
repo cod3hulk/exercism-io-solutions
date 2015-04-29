e803bb95745d20c54a45df278cd4c3ef39ed3be0
Tomas Ave <tomave82@gmail.com>
2015-04-29 20:55:17 +0200

    Refactored initial solution

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -1,14 +1,9 @@
+/**
+ * Compute the hamming distance of two DNA strands
+ */
 object Hamming {
   def compute(strandA: String, strandB: String) = {
-    val min = Math.min(strandA.length, strandB.length)
-    var distance = 0
-    for(i <- 0 to min - 1) {
-      val charA = strandA.charAt(i)
-      val charB = strandB.charAt(i)
-      if(charA != charB) {
-        distance += 1
-      }
-    }
-    distance
+    val minStrandLength = Math.min(strandA.length, strandB.length)
+    0.to(minStrandLength - 1).count(i => strandA.charAt(i) != strandB.charAt(i))
   }
 }
